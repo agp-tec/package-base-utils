48a2bd918d128458a12961ccb77cbce179a8ab9d
francisco <francisco.gomes@ticketmais.com.br>
2020-09-24 23:18:02 -0300

    Commit inicial

--- src/Agp/BaseUtils/Traits/SyncRelations.php
+++ src/Agp/BaseUtils/Traits/SyncRelations.php
@@ -4,7 +4,6 @@
 namespace Agp\BaseUtils\Traits;
 
 
-use Agp\Modelo\Model\Entity\Pivot;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
