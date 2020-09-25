88d3d4e110e36bf08561e3c5c7628cda55906ae1
francisco <francisco.gomes@ticketmais.com.br>
2020-09-24 23:03:09 -0300

    Commit inicial

---
+++ src/Agp/BaseUtils/AgpBaseUtilsServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Agp\Modelo;
+
+use Illuminate\Support\ServiceProvider;
+
+class AgpBaseUtilsServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+    }
+
+    public function register()
+    {
+    }
+}
