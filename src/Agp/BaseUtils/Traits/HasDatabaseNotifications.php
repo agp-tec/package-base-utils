0cca3ae6b36e980a2dd8ddab4d8752cb4495f622
francisco <francisco.gomes@ticketmais.com.br>
2020-10-03 12:09:12 -0300

    Correção de namespace

--- src/Agp/BaseUtils/Traits/HasDatabaseNotifications.php
+++ src/Agp/BaseUtils/Traits/HasDatabaseNotifications.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Traits;
+namespace Agp\BaseUtils\Traits;
 
 use Agp\BaseUtils\Model\Entity\DatabaseNotification;
 
--- src/Agp/BaseUtils/Traits/Notifiable.php
+++ src/Agp/BaseUtils/Traits/Notifiable.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Traits;
+namespace Agp\BaseUtils\Traits;
 
 use Illuminate\Notifications\RoutesNotifications;
 
